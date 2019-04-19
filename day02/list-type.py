c3bc7242bbd7a7c1892b5ad77fe2274a883b30a8
小沫 <1050257224@qq.com>
2019-04-19 11:57:56 +0800

    新增list查询

---
+++ day02/list-type.py
@@ -0,0 +1,11 @@
+def list_sel(a):
+    print(a[2])
+    print(a[-1])
+    print(a[0:2])
+    print(a[2:])
+
+
+if __name__ == '__main__':
+    alist = ['a',4,'nihao','8']
+    list_sel(alist)
+    # print(alist)
